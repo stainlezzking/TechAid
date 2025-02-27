03af7fac3fdfbac82f85e4131d4edbc6f35b386e
Reallife <imhanluohanreallife@gmail.com>
2025-02-28 00:35:12 +0100

    Completed Ticket

--- app/home/ticket/page.jsx
+++ app/home/ticket/page.jsx
@@ -1,6 +1,9 @@
 import Navbar from "@/components/Navbar";
 import Box from "@/components/box";
 import Input from "@/components/input";
+import Button from "@/components/button";
+import Phone from "@/public/phone.png";
+import Email from "@/public/email.png";
 
 const Ticket = function() {
     return (
@@ -10,7 +13,7 @@ const Ticket = function() {
                 <form>
                     <div className="pb-[22px]">
                         <Box className="pl-[60px] pr-[94px] py-[24px] w-full">
-                            <div className="font-medium pb-[10px]">
+                            <div className="font-medium">
                                 Frequently encountered issues
                             </div>
                             <div className="flex flex-col">
@@ -24,15 +27,52 @@ const Ticket = function() {
                         </Box>
                     </div>
                     <div>
-                        <Box className="pl-[70px] pr-[97px] pt-[25px] pb-[36px]">
+                        <Box className="pl-[70px] pr-[97px] py-[25px]">
                         <div className="flex flex-col">
                                 <label className="pb-[17px]">Title:</label>
-                                
                                 <Input 
                                     className="py-[16px] border-none shadow-md"
                                     type="text" 
                                     placeholder="I have isssues logging into my computer">
                                 </Input>
+
+                                <label className="pt-[35px] pb-[15px]">Description:</label>
+                                <textarea className="rounded-md shadow-md px-[5px]" placeholder="Describe your issue in detail"></textarea>
+
+                                <div className="flex pt-[35px] pb-[35px] justify-between">
+                                    <div>
+                                        <span className="pr-[10px]">Select Category:</span>
+                                        <select className="px-[20px] py-[5px] rounded-md shadow-md">
+                                            <option>-----None-----</option>
+                                        </select>
+                                    </div>
+                                   <div>
+                                        <span className="pr-[10px]">Select Priority Level:</span>
+                                        <select className="px-[20px] py-[5px] rounded-md shadow-md">
+                                            <option>-----None-----</option>
+                                        </select>
+                                   </div>
+                                </div>
+
+                                <div className="flex flex-col">
+                                    <span className="font-semibold">Preferred Contact method</span>
+                                    <span>How do you want us to connect with you?</span>
+                                    <div className="flex justify-between">
+                                        <div className="flex gap-[44px]">
+                                            <button className="flex shadow-md rounded-b-lg px-[20px] py-[5px]">
+                                                <img className="pr-[5px]" src={Phone.src} alt="phone"/>
+                                                Phone/Teams Call
+                                            </button>
+                                            <button className="flex shadow-md rounded-b-lg px-[30px] py-[5px]">
+                                                <img className="pr-[5px]" src={Email.src} alt="email"/>
+                                                Email/Teams Chat
+                                            </button>
+                                        </div>
+                                        <div>
+                                            <Button className="px-[20px] py-[5px]">Submit</Button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </Box>
                     </div>
