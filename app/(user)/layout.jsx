5ae5785956ac41bcefe4dd7ee14ce45e5f61b458
Chukwuebuka Stanley <azukachukwuebuka07@gmail.com>
2025-03-08 17:55:11 +0100

    Added auth guard to redirect unlogged in users

--- app/(user)/layout.jsx
+++ app/(user)/layout.jsx
@@ -1,13 +1,16 @@
+import AuthGuard from "@/components/AuthWrapper";
 import Navbar from "@/components/Navbar";
 
 export const dynamic = "force-dynamic";
 
 export default function HomeLayout({ children }) {
   return (
-    <div className="max-xl:mx-31px xl:max-w-[1300px] mx-auto">
-      <Navbar />
-      <div className=" min-h-[calc(100%-50px)] p-[50px] xl:max-w-[1200px] mx-auto">{children}</div>
-      <div className="fixed bottom-0 right-0 left-0 min-w-screen h-[50px] bg-primaryBlue"></div>
-    </div>
+    <AuthGuard>
+      <div className="max-xl:mx-31px xl:max-w-[1300px] mx-auto">
+        <Navbar />
+        <div className=" min-h-[calc(100%-50px)] p-[50px] xl:max-w-[1200px] mx-auto">{children}</div>
+        <div className="fixed bottom-0 right-0 left-0 min-w-screen h-[50px] bg-primaryBlue"></div>
+      </div>
+    </AuthGuard>
   );
 }
